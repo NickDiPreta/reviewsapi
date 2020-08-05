3d02e74031f134fcc3afe6240d4c86395005c3d1
nick <nicholasdipreta2020@gmail.com>
2020-08-05 00:17:05 -0400

    products controller fixed

--- products_reviews_app_api/app/controllers/products_controller.rb
+++ products_reviews_app_api/app/controllers/products_controller.rb
@@ -5,39 +5,17 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
 
-    render json: @products
+    render json: @products, status: 200
   end
 
   # GET /products/1
   def show
-    render json: @product
+    @product = Product.find(params[:id])
+    render json: @product, status: 200
   end
 
   # POST /products
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /products/1
-  def destroy
-    @product.destroy
-  end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
@@ -45,7 +23,3 @@ class ProductsController < ApplicationController
     end
 
     # Only allow a trusted parameter "white list" through.
-    def product_params
-      params.require(:product).permit(:name, :price, :img)
-    end
-end
